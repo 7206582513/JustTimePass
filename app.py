f9aa243bc402973e59391e440d1a013f9f50d17a
7206582513 <rohitmakani564@gmail.com>
2025-05-27 19:18:15 +0530

    change host

--- app.py
+++ app.py
@@ -215,5 +215,10 @@ def download_history():
 
     return send_file(filepath, as_attachment=True)
 
+
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(
+        host='0.0.0.0',
+        port=int(os.environ.get("PORT", 5000)),
+        debug=False  # turn OFF for production
+    )
